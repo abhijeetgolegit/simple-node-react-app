f16f64171cd0f92d4db0f4665c33f12e821ee9cb
abhijeetgolegit <117170792+abhijeetgolegit@users.noreply.github.com>
2023-01-22 14:32:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,11 +7,10 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <p className="App-intro">
-          Let's modify this code
+          Let's play game of Devops here!
         </p>
       </div>
     );
